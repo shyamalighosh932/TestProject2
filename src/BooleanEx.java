d53d7e9a6d2abb7a477a51e4809cf653b309ccdc
shyamalighosh932 <shyamali.ghosh932@gmail.com>
2024-04-21 13:28:21 -0400

    My project

---
+++ src/BooleanEx.java
@@ -0,0 +1,21 @@
+
+public class BooleanEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//boolean isTrue = true;
+		//boolean isFalse = false;
+		
+		boolean isSunny = true;
+		boolean isRainy = false;
+
+		if (isSunny) {
+		    System.out.println("It is sunny today!");
+		}
+
+		if (!isRainy) {
+		    System.out.println("It is not raining.");
+	}
+
+	}
+}
